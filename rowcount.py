d4de055499c2a37bb7c0baa6df9961c64d5e922c
Srikanth <49428234+srikanth-world@users.noreply.github.com>
2023-11-07 13:12:22 +0530

    Update rowcount.py

--- rowcount.py
+++ rowcount.py
@@ -1,6 +1,6 @@
 import openpyxl
 
-def count_data_rows_and_write_to_workbook(file_path):
+def count_rows_and_write_to_sheet(file_path):
     workbook = openpyxl.load_workbook(file_path)
 
     for sheet_name in workbook.sheetnames:
@@ -20,10 +20,13 @@ def count_data_rows_and_write_to_workbook(file_path):
                 elif flag == "QA03 - New":
                     count_new += 1
 
-        worksheet.append(["Count of QA02 - Old Rows", count_old])
-        worksheet.append(["Count of QA03 - New Rows", count_new])
+        last_row = worksheet.max_row
+        worksheet.cell(row=last_row + 1, column=1, value="Count of QA02 - Old Rows")
+        worksheet.cell(row=last_row + 1, column=2, value=count_old)
+        worksheet.cell(row=last_row + 2, column=1, value="Count of QA03 - New Rows")
+        worksheet.cell(row=last_row + 2, column=2, value=count_new)
 
     workbook.save(file_path)
 
 # Replace 'your_file.xlsx' with the path to your Excel file
-count_data_rows_and_write_to_workbook('your_file.xlsx')
+count_rows_and_write_to_sheet('your_file.xlsx')
